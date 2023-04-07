dc0888a92951e77822aa09f2fb2fe78ebeb0ec93
Gleb Sushko <neuroworker@gmail.com>
2023-04-07 11:04:32 +0400

    as issue in vfo is not related to vfo revert back to noto :)

--- tx_panadapter.c
+++ tx_panadapter.c
@@ -140,7 +140,7 @@ void update_tx_panadapter(RADIO *r) {
 
     cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
     cairo_set_line_width(cr, 1.0);
-    cairo_select_font_face(cr, "Andale Mono", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_select_font_face(cr, "Noto Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
     cairo_set_font_size(cr, 10);
     char v[32];
 
